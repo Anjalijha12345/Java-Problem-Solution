7cc81c4eab7a307285e06fc8e41b1d3c85a76dbd
Sadeesha_Jayaweea <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-10-15 12:39:07 +0530

    Recap and Comments Added

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,42 +1,36 @@
-/*Java program to check whether a given number is an Armstrong Number.
- * An armstrong number is a number which is equal to the sum of each of its digits raised to the number
- * of digits. 
- * For example, 
- * 153=1^3+5^3+3^3
- */
-
 import java.util.Scanner;
 
-public class ArmstrongNumber
-{
+public class ArmstrongNumber {
+    // Function to check if a number is an Armstrong number
     public int armstrong(int n) {
+        int num = n, powerSum = 0;  // Initialize variables to store the number and the sum of powered digits
+        String s = Integer.toString(n);  // Convert the number to a string to determine its length
+        int l = s.length();  // Get the number of digits in the input number
         
-        int num = n, powerSum = 0;  
-        String s=Integer.toString(n);
-        int l=s.length();
-        
+        // Loop through each digit of the number
         while (num > 0) {
-            int digit = num % 10;
-            powerSum = (int) (powerSum + (Math.pow(digit,l)));
-            num /= 10;
+            int digit = num % 10;  // Get the last digit of the number
+            powerSum = (int) (powerSum + (Math.pow(digit, l)));  // Add the powered digit to the sum
+            num /= 10;  // Remove the last digit from the number
         }
         
+        // Check if the sum of powered digits is equal to the original number
         if (powerSum == n)
-            return 1;
+            return 1;  // Return 1 if it's an Armstrong number
         else
-            return 0;
+            return 0;  // Return 0 if it's not an Armstrong number
     }
     
     public static void main(String args[]) {
-        
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);  // Create a Scanner object for user input
         System.out.print("Enter Number: ");
-        int num = in.nextInt();
-        in.close();
+        int num = in.nextInt();  // Read an integer from the user
+        in.close();  // Close the input stream
         
-        ArmstrongNumber obj = new ArmstrongNumber();
-        int r = obj.armstrong(num);
+        ArmstrongNumber obj = new ArmstrongNumber();  // Create an instance of the ArmstrongNumber class
+        int r = obj.armstrong(num);  // Call the armstrong method to check if the number is an Armstrong number
         
+        // Display the result to the user
         if (r == 1)
             System.out.println(num + " is an Armstrong number");
         else

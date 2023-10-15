0ddd468e3a6bcb19cdb50c866969acfebba07e10
Sadeesha_Jayaweea <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-10-15 12:46:29 +0530

    Recap With Comments

--- Queue.java
+++ Queue.java
@@ -1,26 +1,23 @@
-/*
- * This java file contains the essential functions required to run the QueueProgram.java program.
- */
 class Queue {
-    private static final int MAX_SIZE = 10;
-    private int[] queueArray;
-    private int front;
-    private int rear;
-    private int size;
+    private static final int MAX_SIZE = 10;  // Maximum size of the queue
+    private int[] queueArray;  // Array to store queue elements
+    private int front;  // Pointer to the front of the queue
+    private int rear;   // Pointer to the rear of the queue
+    private int size;   // Current number of elements in the queue
 
     public Queue() {
-        queueArray = new int[MAX_SIZE];
-        front = -1;
-        rear = -1;
-        size = 0;
+        queueArray = new int[MAX_SIZE];  // Initialize the array with the maximum size
+        front = -1;  // Initialize the front pointer to -1
+        rear = -1;   // Initialize the rear pointer to -1
+        size = 0;    // Initialize the size to 0 (empty queue)
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return size == 0;  // Check if the queue is empty (size is 0)
     }
 
     public boolean isFull() {
-        return size == MAX_SIZE;
+        return size == MAX_SIZE;  // Check if the queue is full (size is equal to the maximum size)
     }
 
     public void enqueue(int item) {
@@ -33,11 +30,11 @@ class Queue {
             front = 0;
             rear = 0;
         } else {
-            rear++;
+            rear++;  // Increment the rear pointer to insert the new element
         }
 
-        queueArray[rear] = item;
-        size++;
+        queueArray[rear] = item;  // Add the new element to the rear of the queue
+        size++;  // Increment the size
         System.out.println("Enqueued: " + item);
     }
 
@@ -47,16 +44,16 @@ class Queue {
             return -1;
         }
 
-        int item = queueArray[front];
-        front++;
-        size--;
+        int item = queueArray[front];  // Get the element at the front of the queue
+        front++;  // Increment the front pointer to remove the front element
+        size--;   // Decrement the size
 
         if (front > rear) {
             front = -1;
-            rear = -1;
+            rear = -1;  // Reset both pointers when the queue becomes empty
         }
 
-        return item;
+        return item;  // Return the dequeued element
     }
 
     public int peek() {
@@ -65,7 +62,7 @@ class Queue {
             return -1;
         }
 
-        return queueArray[front];
+        return queueArray[front];  // Get the element at the front of the queue without dequeuing
     }
 
     public void display() {
@@ -76,7 +73,7 @@ class Queue {
 
         System.out.print("Queue: ");
         for (int i = front; i <= rear; i++) {
-            System.out.print(queueArray[i] + " ");
+            System.out.print(queueArray[i] + " ");  // Display all elements in the queue from front to rear
         }
         System.out.println();
     }
